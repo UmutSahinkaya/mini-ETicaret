41f393d0b4877b51a12d496fd0d8861cce7cdfa9
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-02-14 14:02:39 +0300

    toaster testi bittiği için app.component.ts den sildim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import {
-  CustomToastrService,
-  ToastrMessageType,
-  ToastrPosition,
-} from './services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-root',
@@ -13,24 +7,7 @@ import {
 })
 export class AppComponent {
   title = 'ETicaret.Client';
-  constructor(private toastrService: CustomToastrService) {
-    toastrService.message(
-      'Merhaba',
-      'Umut',
-      {messageType:ToastrMessageType.Success,
-      position:ToastrPosition.BottomFullWidth}
-    );
-    toastrService.message('Merhaba', 'Umut', {
-      messageType: ToastrMessageType.Info,
-      position: ToastrPosition.TopCenter,
-    });
-    toastrService.message('Merhaba', 'Umut', {
-      messageType: ToastrMessageType.Warning,
-      position: ToastrPosition.TopLeft,
-    });
-    toastrService.message('Merhaba', 'Umut', {
-      messageType: ToastrMessageType.Error,
-      position: ToastrPosition.TopRight,
-    });
+  constructor() {
+    
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { UiModule } from './ui/ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     AdminModule,
     UiModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
