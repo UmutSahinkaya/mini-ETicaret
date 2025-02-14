8acebf18a6aff77e530b80f02ea9391c5e600b85
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-02-14 10:42:54 +0300

    Components ve Model mimarisi oluşturuldu . Admin ve Ui kısımları ayrıldı. Ekstra paketler(Alertify,Material,Bootstrap vs ) yüklendi son durum bu şekilde commit atıyorum.

---
+++ src/app/admin/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor(private alertify:AlertifyService){}
+  
+  
+  ngOnInit(): void {
+    this.alertify.message("Merhaba",MessageType.Notify)
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //BrowserAnimationsModule,
+    AppRoutingModule,
+    AdminModule,
+    UiModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
