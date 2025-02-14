9da84b762f6da8b1f1e8635e0b6019f7322e9cad
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-02-14 11:53:52 +0300

    alertify test ederken kullandığım yerlerden temizledim.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AlertifyService,
-  MessageType,
-  Position,
-} from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,18 +6,8 @@ import {
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private alertify: AlertifyService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-  m() {
-    this.alertify.message(
-      'Merhaba',
-      MessageType.Notify,
-      Position.BottomRight,
-      5,true
-    );
-  }
-  d(){
-    this.alertify.dismiss();
-  }
+  
 }
--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -12,7 +12,7 @@ export class LayoutComponent implements OnInit {
   
   
   ngOnInit(): void {
-    //this.alertify.message("Merhaba",MessageType.Notify,Position.BottomRight,5)
+    
   }
 
 }
