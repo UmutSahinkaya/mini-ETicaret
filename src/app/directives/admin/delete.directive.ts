129cc3aa3bd83314e0de863b8f5d31b92e64b587
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-02-16 21:12:29 +0300

    Delete Directive düzeltmeleri

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import {
   Directive,
   ElementRef,
@@ -14,6 +15,8 @@ import {
   DeleteDialogComponent,
   DeleteState,
 } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 declare var $: any;
@@ -25,9 +28,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
+    private httpClientService: HttpClientService,
     private spinner: NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private alertifyService:AlertifyService
   ) {
     const img = _renderer.createElement('img');
     img.setAttribute('src', '../../../../../assets/delete.png');
@@ -38,21 +42,45 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string;
   @Output() callback: EventEmitter<any> = new EventEmitter();
   @HostListener('click')
   async onClick() {
     this.openDialog(async () => {
       this.spinner.show(SpinnerType.BallAtom);
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate({
-        opacity:0,
-        left:"+=50",
-        height:"toggle"
-      },700,()=>{
-        this.callback.emit();
-      })
-      
+      this.httpClientService
+        .delete(
+          {
+            controller: this.controller,
+          },
+          this.id
+        )
+        .subscribe((data) => {
+          $(td.parentElement).animate(
+            {
+              opacity: 0,
+              left: '+=50',
+              height: 'toggle',
+            },
+            700,
+            () => {
+              this.callback.emit();
+              this.alertifyService.message("Ürün başarıyla silinmiştir.",{
+                dismissOther:true,
+                messageType:MessageType.Success,
+                position:Position.TopRight
+              });
+            }
+          );
+        },(errorResponse:HttpErrorResponse)=>{
+          this.spinner.hide(SpinnerType.BallAtom);
+          this.alertifyService.message("Ürün silinirken bir hata ile karşılaşılmıştır.", {
+            dismissOther: true,
+            messageType: MessageType.Error,
+            position: Position.BottomCenter,
+          });
+        });
     });
   }
   openDialog(afterClosed: any): void {
